308a1116c7d3f8a6f76812f1f9f62da9511b2810
Antonio Rafael Braga <rafaeltelematica@gmail.com>
2025-06-07 13:24:23 -0300

    organizando

--- Capitulo5/secao_5_3/rpc-java/rpc-python/hello_service.py
+++
@@ -1,7 +0,0 @@
-# Implementação de RPC (Remote Procedure Call) com sockets em Python
-
-# Arquivo: hello_service.py
-# Define a interface e implementação do serviço
-class HelloService:
-    def say_hello(self, name):
-        return f"Olá, {name}! Este é um exemplo de RPC com sockets em Python."
