cdede33db5d0ba38cc02e8f12f13a8fc6196bed0
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2025-10-08 21:39:55 -0300

    Fix

---
+++ Capitulo9/api/HelloResource.java
@@ -0,0 +1,22 @@
+package main.java.com.exemplo;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloResource {
+
+    @Inject
+    private HelloService helloService;
+
+    @GET
+    @Path("/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayHello(@PathParam("name") String name) {
+        return helloService.sayHello(name);
+    }
+}
---
+++ Capitulo9/api/RestApplication.java
@@ -0,0 +1,9 @@
+package main.java.com.exemplo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+// Define o caminho base da API REST
+@ApplicationPath("/api")
+    public class RestApplication extends Application {
+}
